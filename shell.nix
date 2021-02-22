fa92640e69486938c13ccd5330f9829cd4b0b01e
Robbert Gurdeep Singh <git@beardhatcode.be>
2021-02-22 17:55:57 +0100

    Clean nix shell

--- shell.nix
+++ shell.nix
@@ -3,29 +3,38 @@ let
   needed = with pkgs; [
     stack
     ghc
-    clang-tools
+    clang-tools # for C formatting
     nodePackages.prettier
-    haskellPackages.profiteur
+    haskellPackages.profiteur # treemap of profile
   ];
-  stack-run = pkgs.writeShellScriptBin "stack-profile" ''
-    PATH=${pkgs.lib.makeBinPath needed}:"$PATH"
-    if [ -z $1 ]; then echo "give a program path!";exit 1; fi
-    set -ex
-    stack build
-    cat $1 | stack exec gaiwan-exe
-  '';
-  stack-profile = pkgs.writeShellScriptBin "stack-profile" ''
-    PATH=${pkgs.lib.makeBinPath needed}:"$PATH"
-    if [ -z $1 ]; then echo "Give a program path" ;exit 1; fi
-    set -ex
-    stack clean
-    stack build --profile
-    cat $1 | stack exec --profile gaiwan-exe --RTS -- +RTS -p
-    profiteur gaiwan-exe.prof
-  '';
 in
 pkgs.mkShell {
   buildInputs = needed ++ [
-    stack-profile
+    (pkgs.writeShellScriptBin
+      "stack-watch-test"
+      ''
+        PATH=${pkgs.lib.makeBinPath needed}:"$PATH"
+        stack test --file-watch
+      '')
+    (pkgs.writeShellScriptBin
+      "stack-run"
+      ''
+        PATH=${pkgs.lib.makeBinPath needed}:"$PATH"
+        if [ -z $1 ]; then echo "give a program path!";exit 1; fi
+        set -ex
+        stack build
+        cat $1 | stack exec gaiwan-exe
+      '')
+    (pkgs.writeShellScriptBin
+      "stack-profile"
+      ''
+        PATH=${pkgs.lib.makeBinPath needed}:"$PATH"
+        if [ -z $1 ]; then echo "Give a program path" ;exit 1; fi
+        set -ex
+        stack clean
+        stack build --profile
+        cat $1 | stack exec --profile gaiwan-exe --RTS -- +RTS -p
+        profiteur gaiwan-exe.prof
+      '')
   ];
 }
